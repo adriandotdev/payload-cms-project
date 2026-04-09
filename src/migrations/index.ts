95dcc2ac1ee7fded715f97faa43c2a4bb55bf882
adriandotdev <marcelonads@gmail.com>
2026-04-09 20:19:40 +0800

    generate migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260409_121530 from './20260409_121530';
+
+export const migrations = [
+  {
+    up: migration_20260409_121530.up,
+    down: migration_20260409_121530.down,
+    name: '20260409_121530'
+  },
+];
