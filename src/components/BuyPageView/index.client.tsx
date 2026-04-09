541630319f9ff5ab99e07d1ff9c9cbff2730f307
adriandotdev <marcelonads@gmail.com>
2026-04-10 02:42:46 +0800

    add index client

--- src/components/BuyPageView/index.client.tsx
+++ src/components/BuyPageView/index.client.tsx
@@ -1,21 +1,40 @@
 'use client'
 
 import { ProductsSelect } from '@/payload-types'
+import { Where } from 'payload'
+import { stringify } from 'qs-esm'
 import { useEffect, useState } from 'react'
 
 export const BuyPageClient: React.FC = () => {
-  const [products, setProducts] = useState<ProductsSelect | []>([])
+  const [search, setSearch] = useState('')
+  const [products, setProducts] = useState<ProductsSelect[]>([])
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const result = await fetch('/api/products')
+    const handler = setTimeout(() => {
+      const query: Where = {
+        name: {
+          contains: search,
+        },
+      }
 
-      const data = await result.json()
-      console.log(data)
-    }
+      const stringifiedQuery = stringify({ where: query }, { addQueryPrefix: true })
 
-    void fetchProducts()
-  }, [])
+      const fetchProducts = async () => {
+        const result = await fetch(`/api/products${stringifiedQuery}`)
+        const data = await result.json()
+        setProducts(data.docs ?? [])
+      }
 
-  return <div>Hello from client</div>
+      void fetchProducts()
+    }, 300)
+
+    return () => clearTimeout(handler)
+  }, [search])
+
+  return (
+    <div>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <p>{JSON.stringify(products)}</p>
+    </div>
+  )
 }
